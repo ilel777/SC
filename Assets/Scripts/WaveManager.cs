2ca1e032416690b346096bca389791324a7a6568
ilel <magnum.sadler@gmail.com>
2019-12-17 18:31:53 +0100

    Add basic wave manager

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField]
+    float waveSpawnZ, waveSpawnXOffset;
+    [SerializeField]
+    GameObject asteroid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveSpawnZ = 7.0f;
+        asteroid = Resources.Load("Prefabs/asteroid") as GameObject;
+        InvokeRepeating("SpawnWave", 2.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnWave(){
+        SpawnItem(asteroid);
+    }
+
+    void SpawnItem(GameObject item){
+        Vector3 itemSpawnPostition = new Vector3(Random.Range(-1,1), 0, waveSpawnZ);
+        Instantiate(item, itemSpawnPostition, Quaternion.identity);
+    }
+}
