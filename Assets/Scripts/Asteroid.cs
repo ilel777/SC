01036b0dfb7884eec7109878b5f24cb6cc993beb
ilel <magnum.sadler@gmail.com>
2019-12-17 23:01:36 +0100

    Add asteroid rotation and refine the wave manager

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    private Rigidbody rb;
+    [SerializeField]
+    private float rotationSpeed;
+    private Vector3 torqueVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        torqueVector = Random.insideUnitSphere.normalized;
+        rotationSpeed = 0.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddTorque(torqueVector * rotationSpeed * rb.mass);
     }
 }
--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    [SerializeField]
     float waveSpawnZ, waveSpawnXOffset;
-    [SerializeField]
-    GameObject asteroid;
+    List<GameObject> spaceObjects = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         waveSpawnZ = 7.0f;
-        asteroid = Resources.Load("Prefabs/asteroid") as GameObject;
-        InvokeRepeating("SpawnWave", 2.0f, 1.0f);
+        waveSpawnXOffset = 2.0f;
+        spaceObjects.AddRange(Resources.LoadAll<GameObject>("Prefabs/SpaceItems"));
+        InvokeRepeating("SpawnWave", 2.0f, 1.5f);
     }
 
     // Update is called once per frame
@@ -23,12 +22,14 @@ public class WaveManager : MonoBehaviour
 
     }
 
-    void SpawnWave(){
-        SpawnItem(asteroid);
+    void SpawnWave()
+    {
+        SpawnItem(spaceObjects[Random.Range(0, 3)]);
     }
 
-    void SpawnItem(GameObject item){
-        Vector3 itemSpawnPostition = new Vector3(Random.Range(-1,1), 0, waveSpawnZ);
+    void SpawnItem(GameObject item)
+    {
+        Vector3 itemSpawnPostition = new Vector3(Random.Range(-1.0f, 1.0f) * waveSpawnXOffset, 0, waveSpawnZ);
         Instantiate(item, itemSpawnPostition, Quaternion.identity);
     }
 }
