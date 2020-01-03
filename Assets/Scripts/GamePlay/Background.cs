babbee8151f938df039331808c16637a72e012b5
ilel <magnum.sadler@gmail.com>
2020-01-03 20:35:54 +0100

    Add Scrolling background

---
+++ Assets/Scripts/GamePlay/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField]
+    float _scrollSpeed, _tileSizeZ;
+
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * _scrollSpeed, _tileSizeZ);
+        transform.position = _startPosition - Vector3.forward * newPosition;
+    }
+}
