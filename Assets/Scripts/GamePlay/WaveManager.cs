757a093add137404688ffaddd7651e021fc65cbd
ilel <magnum.sadler@gmail.com>
2020-01-18 15:53:52 +0100

    remake wavemanager

--- Assets/Scripts/GamePlay/WaveManager.cs
+++ Assets/Scripts/GamePlay/WaveManager.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    List<Pool<GameObject>> _obstaclesPools = new List<Pool<GameObject>>();
+
     float waveSpawnZ, waveSpawnXOffset;
-    List<Pool<GameObject>> spaceObjectPools = new List<Pool<GameObject>>();
 
     int _objectSpawned;
     int _waveNumber;
@@ -14,18 +16,28 @@ public class WaveManager : MonoBehaviour
     // stores the max width and height of objects to be spawned
     float _maxWidth, _maxHeight;
 
+
+    // support waiting after spawning an item
+    private float _spawnItemWait;
+    // support waiting after spawning a wave of items
+    Timer _spawnWaveWait;
+    // support waiting at the start of level before spawning items
+    Timer _startWait;
+
+
     // Start is called before the first frame update
     void Start()
     {
         waveSpawnZ = ScreenUtils.ScreenTop + (ScreenUtils.ScreenTop / 5);
         waveSpawnXOffset = ScreenUtils.ScreenRight;
-        // spaceObjects.AddRange(Resources.LoadAll<GameObject>("Prefabs/SpaceItems"));
-        spaceObjectPools = new List<Pool<GameObject>>();
-        spaceObjectPools.Add(PoolsContainer.Enemies);
-        spaceObjectPools.Add(PoolsContainer.Asteroids);
-        spaceObjectPools.Add(PoolsContainer.Powerups);
 
-        foreach (Pool<GameObject> pool in spaceObjectPools)
+        // spaceObjects.AddRange(Resources.LoadAll<GameObject>("Prefabs/SpaceItems"));
+        _obstaclesPools = new List<Pool<GameObject>>();
+        _obstaclesPools.Add(PoolsContainer.Enemies);
+        _obstaclesPools.Add(PoolsContainer.Asteroids);
+        _obstaclesPools.Add(PoolsContainer.Powerups);
+
+        foreach (Pool<GameObject> pool in _obstaclesPools)
         {
             GameObject obj = pool.Get();
             float width = obj.GetComponent<ISize>().GetWidth();
@@ -42,32 +54,65 @@ public class WaveManager : MonoBehaviour
                 _maxHeight = height;
             }
         }
-        InvokeRepeating("SpawnWave", 1.0f, 1.0f);
+        // InvokeRepeating("SpawnWave", 1.0f, 1.0f);
+
+        _spawnItemWait = 1.0f;
+        _startWait = gameObject.AddComponent<Timer>();
+        _startWait.Duration = 0.5f;
+        _spawnWaveWait = gameObject.AddComponent<Timer>();
+        _spawnWaveWait.Duration = 1.5f;
+
+        _startWait.AddTimerFinishedEventListener(StartSpawnWave);
+        _spawnWaveWait.AddTimerFinishedEventListener(StartSpawnWave);
+
+        _startWait.Run();
     }
 
-    void SpawnWave()
+    private void StartSpawnWave()
     {
-        Pool<GameObject> pool = spaceObjectPools[Random.Range(0, spaceObjectPools.Count)];
-        SpawnItem(pool.Get());
-        if (++_objectSpawned == 5)
+        StartCoroutine(SpawnWave());
+    }
+
+    IEnumerator SpawnWave()
+    {
+        int itemsNumber = 5;
+
+        while (itemsNumber > 0)
         {
-            _waveNumber++;
-            _speedScale = 0.1f * _waveNumber;
-            Debug.Log("Movement Speed: " + _speedScale);
-            _objectSpawned = 0;
+            // spawn item
+            if (SpawnItem()) itemsNumber--;
+
+            // wait
+            if (itemsNumber != 0)
+                yield return new WaitForSeconds(_spawnItemWait);
         }
+
+        _spawnWaveWait.Run();
+        yield return null;
     }
 
-    void SpawnItem(GameObject item)
-    {
-        Movement movement = item.GetComponent<Movement>();
 
-        movement.Speed += (movement.Speed * _speedScale);
+    GameObject SpawnItem()
+    {
+        Pool<GameObject> pool = _obstaclesPools[UnityEngine.Random.Range(0, _obstaclesPools.Count)];
+        GameObject item = pool.Get();
+
+        // Movement movement = item.GetComponent<Movement>();
+        // movement.Speed += (movement.Speed * _speedScale);
 
         Vector3 itemSpawnPostition = SpawnPosition(item.GetComponent<ISize>());
         if (itemSpawnPostition != Vector3.zero)
+        {
             item.transform.position = itemSpawnPostition;
+            return item;
+        }
+        else
+        {
+            pool.Return(item);
+            return null;
+        }
         // Instantiate(item, itemSpawnPostition, Quaternion.identity);
+
     }
 
     /// <summary>
@@ -77,7 +122,7 @@ public class WaveManager : MonoBehaviour
     {
         for (int i = 0; i < 10; ++i)
         {
-            float x_pos = Random.Range(-1.0f, 1.0f) * waveSpawnXOffset;
+            float x_pos = UnityEngine.Random.Range(-1.0f, 1.0f) * waveSpawnXOffset;
             if (x_pos + (item.GetWidth() / 2) < ScreenUtils.ScreenRight && x_pos - (item.GetWidth() / 2) > ScreenUtils.ScreenLeft)
             {
                 return new Vector3(x_pos, 0, ScreenUtils.ScreenTop + item.GetHeight());
@@ -85,4 +130,5 @@ public class WaveManager : MonoBehaviour
         }
         return Vector3.zero;
     }
+
 }
